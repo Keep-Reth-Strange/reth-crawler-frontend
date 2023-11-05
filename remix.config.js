ed1a62133d5145ee83a5ef3e4bb8056622aa97eb
maschad96 <matthew.schad@consensys.net>
2023-11-05 12:42:10 -0800

    fix: get rid of netlify specific things

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,6 @@
-import { config } from "@netlify/remix-adapter";
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ...(process.env.NODE_ENV === "production" ? config : undefined),
   ignoredRouteFiles: ["**/.*"],
   serverDependenciesToBundle: ["@amcharts/amcharts5/percent", "d3-shape", "d3-path", "@amcharts/amcharts5", "svg-arc-to-cubic-bezier", "@amcharts/amcharts5/themes/Responsive", "@amcharts/amcharts5/themes/Animated", "@amcharts/amcharts5/themes/Dark"],
   // appDirectory: "app",
